3b7be5f996a66ab7db7a3df4ee2b091dcfe648e2
Afshin Mehrabani <afshin.meh@gmail.com>
2026-02-02 17:22:19 +0000

    fix(client): remove inline

--- packages/client/src/components/AskAI.tsx
+++ packages/client/src/components/AskAI.tsx
@@ -4,7 +4,6 @@ import { AskAI as AskAIDefault } from '@/ask-ai/ask-ai';
 import { AskAIChat } from '@/ask-ai/chat';
 import { AskAIDialog } from '@/ask-ai/dialog';
 import { AskAIHeader } from '@/ask-ai/header';
-import { AskAIInline } from '@/ask-ai/inline';
 import { AskAIInput } from '@/ask-ai/input';
 import { AskAIMessages } from '@/ask-ai/messages';
 import { AskAIRoot } from '@/ask-ai/root';
@@ -22,14 +21,12 @@ export const AskAI = Object.assign(AskAIDefault, {
   Messages: AskAIMessages,
   Suggestions: AskAISuggestions,
   Input: AskAIInput,
-  Inline: AskAIInline,
 });
 
 export type { AskAIProps } from '@/ask-ai/ask-ai';
 export type { AskAIChatProps } from '@/ask-ai/chat';
 export type { AskAIDialogProps } from '@/ask-ai/dialog';
 export type { AskAIHeaderProps } from '@/ask-ai/header';
-export type { AskAIInlineProps } from '@/ask-ai/inline';
 export type { AskAIInputProps } from '@/ask-ai/input';
 export type { AskAIMessagesProps } from '@/ask-ai/messages';
 export type { AskAIProviderProps } from '@/ask-ai/provider';
